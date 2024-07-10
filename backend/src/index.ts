587f7047f6c549b9f02c7f84b1ff0ddf1d3436f7
brxshncy <ggkidsbruce619@gmail.com>
2024-07-10 21:24:59 +0800

    wip

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+// Initialize express, mongoose, cors, dotenv
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+// Middleware that converts the body from the request sent to server to JSON
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({
+    message: "test",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT: ${PORT}`);
+});
